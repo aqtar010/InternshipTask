4daf78a305076c431590e2dcf3b030b2ad73305d
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-07-05 11:20:13 +0530

    updated

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ async function main() {
 main().catch((error) => {
   console.error('Error:', error);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Future optimizations:
+This application is not totally secure,one thing is to look in to the security
+it commucates with the gmail api many time , this should be looked into 
+getting unnessary data during an api call
+there may be a better approach than applied here
+*/
